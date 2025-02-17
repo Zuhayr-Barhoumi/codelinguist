26283104dd269d4b93bd68056347f78fd667d573
Zuhayr-Barhoumi <zouheir.barhoumi@gmail.com>
2025-02-17 19:53:30 +0100

    working mock pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), vue()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
 });
